015a5a2b4f81fbc2eca2f9780846658e90c277be
Heloisa <heloisa@desrotulando.com>
2023-03-08 16:05:47 -0300

    cria as funções de leitura das informações armazenadas

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
+import 'package:sqflite/sqflite.dart';
 import 'package:task_project/components/taskcard.dart';
+import 'package:task_project/data/database.dart';
 // O DAO (Data Acess Object) é uma forma de org. o db. Interface entre flutter e SQL.
 
 class TaskDao {
@@ -12,8 +14,37 @@ class TaskDao {
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
 
+  Future<List<TaskCard>> findAll() async {
+    print('findAll');
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db.query(_tableName);
+    print("result: $result");
+    return toList(result);
+  }
+
+  List<TaskCard> toList(List<Map<String, dynamic>> mapaDeTarefas) {
+    print('em to list');
+    final List<TaskCard> tarefas = [];
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
+      final TaskCard tarefa = TaskCard(
+        text: linha[_name],
+        difficulty: linha[_difficulty],
+        image: linha[_image],
+      );
+      tarefas.add(tarefa);
+    }
+    print('lista de tarefas $tarefas');
+    return tarefas;
+  }
+
+  Future<List<TaskCard>> find(String nomeDaTarefa) async {
+    print('find');
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> result = await db
+        .query(_tableName, where: '$_name = ?', whereArgs: [nomeDaTarefa]);
+    print("tarefa encontrada ${toList(result)}");
+    return toList(result);
+  }
   // save(TaskCard task) async {}
-  // Future<List<TaskCard>> findAll() async {}
-  // Future<List<TaskCard>> find(String nomeDaTarefa)
   // delete(String nomeDaTarefa) async {return }
 }
